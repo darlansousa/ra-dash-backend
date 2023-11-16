d967e790fd2d3b0a4a851a89791a0e3b8514c546
Darlan Souza <darlansousa.ds@gmail.com>
2023-11-15 21:24:51 -0300

    development api

--- controllers/complaints.js
+++ controllers/complaints.js
@@ -1,48 +1,161 @@
 const getComplaints = (req, res, db) => {
-    db.select('*').from('complaints')
-      .then(items => {
-        if(items.length){
-          res.json(items)
-        } else {
-          res.json({dataExists: 'false'})
-        }
-      })
-      .catch(err => res.status(400).json({dbError: 'db error:' + err}))
-  }
-  
-  const postComplaints = (req, res, db) => {
-    const { ra_cod, ra_id, title, date_description, chanel, reason, description, complainer_id } = req.body
-    const added = new Date()
-    db('complaints').insert({ra_cod, ra_id, title, date_description, chanel, reason, description, complainer_id})
-      .returning('*')
-      .then(item => {
-        res.json(item)
-      })
-      .catch(err => res.status(400).json({dbError: 'db error'}))
-  }
-  
-  const putComplaints = (req, res, db) => {
-    const { id, ra_cod, ra_id, title, date_description, chanel, reason, description, complainer_id } = req.body
-    db('complaints').where({id}).update({ ra_cod, ra_id, title, date_description, chanel, reason, description, complainer_id })
-      .returning('*')
-      .then(item => {
-        res.json(item)
-      })
-      .catch(err => res.status(400).json({dbError: 'db error'}))
-  }
-  
-  const deleteComplaints = (req, res, db) => {
-    const { id } = req.body
-    db('complaints').where({id}).del()
-      .then(() => {
-        res.json({delete: 'true'})
-      })
-      .catch(err => res.status(400).json({dbError: 'db error'}))
-  }
-  
-  module.exports = {
-    getComplaints,
-    postComplaints,
-    putComplaints,
-    deleteComplaints
-  }
+  const {status, sys_reason, ai_classification} = req.query
+  db.select('*').from('complaints')
+    .innerJoin('complainers', 'complainers.id', 'complaints.complainer_id')
+    .where((qb) => {
+      if (status) {
+        qb.orWhere('complaints_status', '=', status);
+      }
+
+      if (sys_reason) {
+        qb.orWhere('system_sub_reason', '=', sys_reason);
+      }
+
+      if (ai_classification) {
+        qb.orWhere('ai_classification', '=', ai_classification);
+      }
+    })
+    .then(items => {
+      if (items.length) {
+        res.json(items)
+      } else {
+        res.json([])
+      }
+    })
+    .catch(err => res.status(400).json({ dbError: 'db error:' + err }))
+}
+
+const getComplaintsInfo = (req, res, db) => {
+
+  var all = db.from('complaints')
+  .count('*')
+  .as('all');
+
+  var pending = db.from('complaints')
+  .count('*').where({'complaints_status': 'pending'})
+  .as('pending');
+
+  var closed = db.from('complaints')
+  .count('*').where({'complaints_status': 'closed'})
+  .as('closed');
+
+  var ai_review = db.from('complaints')
+  .count('*').whereNot({'ai_classification': null})
+  .as('ai_review');
+
+  var pending_process = db.from('complaints_process')
+  .count('*').where({'status': 'pending'})
+  .as('pending_process');
+   
+  db.select(all, closed, pending, ai_review, pending_process).from('complaints').limit(1)
+    .then(items => {
+        res.json(items[0])
+    })
+    .catch(err => res.status(400).json({ dbError: 'db error:' + err }))
+}
+
+const getComplaintsById = (req, res, db) => {
+  const id = req.params.id
+  db.select('*').from('complaints')
+    .innerJoin('complainers', 'complainers.id', 'complaints.complainer_id')
+    .where({ "complaints.id": id })
+    .then(items => {
+      if (items.length) {
+        res.json(items[0])
+      } else {
+        res.json({ dataExists: 'false' })
+      }
+    })
+    .catch(err => res.status(400).json({ dbError: 'db error:' + err }))
+}
+
+const putAllData = (req, res, db) => {
+  const id = req.params.id
+  const {
+    ra_cod,
+    ra_id,
+    title,
+    date_description,
+    chanel,
+    reason,
+    description,
+    id_occurrence,
+    close_date,
+    system_sub_reason,
+    complainer_note,
+    complaints_status,
+    ai_classification,
+    negotiate_again
+  } = req.body
+  const close_date_db = close_date.split('T')[0]
+  const {
+    complainer_id,
+    name,
+    cpf,
+    uc,
+    city,
+    email,
+    phone,
+    is_client } = req.body
+  db('complaints').where({ id }).update({
+    ra_cod,
+    ra_id,
+    title,
+    date_description,
+    chanel,
+    reason,
+    description,
+    id_occurrence,
+    close_date: close_date_db,
+    system_sub_reason,
+    complainer_note,
+    complaints_status,
+    ai_classification,
+    negotiate_again
+  })
+    .then(() => {
+      db('complainers').where({ id: complainer_id }).update({
+        name,
+        cpf,
+        uc,
+        city,
+        email,
+        phone,
+        is_client })
+        .then(() => getComplaintsById(req, res, db))
+        .catch(err => res.status(400).json({ dbError: 'db error update user' + err}))
+    })
+    .catch(err => res.status(400).json({ dbError: 'db error update complaint' + err }))
+}
+
+const closeComplaints = (req, res, db) => {
+  const id = req.params.id
+  const { id_occurrence, close_date, system_sub_reason, complainer_note, negotiate_again } = req.body
+
+  const close_date_db = close_date.split('T')[0]
+
+  db('complaints').update({ id_occurrence, close_date: close_date_db, system_sub_reason, complainer_note, negotiate_again, complaints_status: 'closed' })
+    .where({ id })
+    .then(() => {
+      getComplaintsById(req, res, db)
+    })
+    .catch(err => res.status(400).json({ dbError: 'db error' + err }))
+}
+
+const deleteComplaints = (req, res, db) => {
+  const { id } = req.body
+  db('complaints').where({ id }).del()
+    .then(() => {
+      res.json({ delete: 'true' })
+    })
+    .catch(err => res.status(400).json({ dbError: 'db error' }))
+}
+
+module.exports = {
+  getComplaints,
+  getComplaintsInfo,
+  getComplaintsById,
+  deleteComplaints,
+  closeComplaints,
+  putAllData
+}
--- server.js
+++ server.js
@@ -31,10 +31,10 @@ var db = require('knex')({
 const complaints = require('./controllers/complaints')
 const complainers = require('./controllers/complainers')
 
-// App
+
 const app = express()
 
-// App Middleware
+
 const whitelist = ['http://localhost:3001']
 const corsOptions = {
   origin: function (origin, callback) {
@@ -50,12 +50,13 @@ app.use(cors(corsOptions))
 app.use(bodyParser.json())
 app.use(morgan('combined')) 
 
-// App Routes - Auth
 app.get('/', (req, res) => res.send('RA-Dash-Backend'))
 
 app.get('/complaints', (req, res) => complaints.getComplaints(req, res, db))
-app.post('/complaints', (req, res) => complaints.postComplaints(req, res, db))
-app.put('/complaints', (req, res) => complaints.putComplaints(req, res, db))
+app.get('/complaints/info', (req, res) => complaints.getComplaintsInfo(req, res, db))
+app.get('/complaints/:id', (req, res) => complaints.getComplaintsById(req, res, db))
+app.put('/complaints/:id/close', (req, res) => complaints.closeComplaints(req, res, db))
+app.put('/complaints/:id', (req, res) => complaints.putAllData(req, res, db))
 app.delete('/complaints', (req, res) => complaints.deleteComplaints(req, res, db))
 
 
@@ -64,7 +65,7 @@ app.post('/complainers', (req, res) => complainers.postComplainers(req, res, db)
 app.put('/complainers', (req, res) => complainers.putComplainers(req, res, db))
 app.delete('/complainers', (req, res) => complainers.deleteComplainers(req, res, db))
 
-// App Server Connection
+
 app.listen(process.env.PORT || 3000, () => {
   console.log(`app is running on port ${process.env.PORT || 3000}`)
 })
